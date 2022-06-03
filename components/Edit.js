e072cd03a5bc7d67ad79c55a4bbe089d4499cf2f
lyongaming <owlsmouth@gmail.com>
2022-06-03 18:13:49 -0400

    Basic testing of the functionality, not too much

--- components/Edit.js
+++ components/Edit.js
@@ -1,14 +1,49 @@
 const template = document.createElement("template");
 template.innerHTML = `
-    <input id="edit-button" type="number"></input>
+    <input id="edit-btn" type="number"></input>
 `;
 
 class Edit extends HTMLElement {
 
     constructor() {
         super();
+        this.value = this.hasAttribute("value") ? this.getAttribute("value") : 0;
+    }
+
+    
+    connectedCallback() {
         this.attachShadow({ mode: "open" });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        const input = this.shadowRoot.getElementById("edit-btn");
+        input.setAttribute("value", this.value);
+        input.addEventListener("change", e => this.changeValue.bind(Edit.prototype, e)());
+    }
+    
+    changeValue(e) {
+        this.attributeChangedCallback("value", this.value, e.target.value);
+        console.log(this.compareValues("2"));
+    }
+    
+    static get observedAttributes() {
+        return ["value"];
+    }
+    
+    attributeChangedCallback(property, oldValue, newValue) {
+    
+        if(oldValue === newValue) return;
+        this[property] = newValue;
+
+    }
+    
+    isEqual(value) {
+        return this.value === value;
+    }
+
+    compareValues(value) {
+        const nums = [ this.value, value ];
+        const [ num1, num2 ] = nums.map(num => parseInt(num))
+        return num1 - num2;
     }
 
 }
